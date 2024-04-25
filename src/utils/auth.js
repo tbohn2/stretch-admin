94e0e367b578f43c48aadeacf652dc986aa9bc72
tbohn2 <tanner.bohn@yahoo.com>
2024-04-25 11:26:08 -0700

    Created auth.js to handle JWT for login status

---
+++ src/utils/auth.js
@@ -0,0 +1,37 @@
+import { jwtDecode } from 'jwt-decode';
+
+class AuthService {
+    getProfile() {
+        return jwtDecode(this.getToken());
+    }
+
+    loggedIn() {
+        const token = this.getToken();
+        return token && !this.isTokenExpired(token) ? true : false;
+    }
+
+    isTokenExpired(token) {
+        const decoded = jwtDecode(token);
+        if (decoded.exp < Date.now() / 1000) {
+            localStorage.removeItem('id_token');
+            return true;
+        }
+        return false;
+    }
+
+    getToken() {
+        return localStorage.getItem('id_token');
+    }
+
+    login(idToken) {
+        localStorage.setItem('id_token', idToken);
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+    }
+}
+
+const auth = new AuthService();
+
+export default auth;
