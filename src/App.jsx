2c488de785635e29a532730f42df672abc49951a
tbohn2 <tanner.bohn@yahoo.com>
2024-04-25 11:26:43 -0700

    Added login page that displays if not logged in

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import CalendarDisplay from './pages/Calendar';
 import Clients from './pages/Clients';
+import Login from './pages/Login';
+import auth from './utils/auth';
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(auth.loggedIn());
   const [calendarChecked, setCalendarChecked] = useState(true);
   const [clientsChecked, setClientsChecked] = useState(false);
 
@@ -21,23 +24,27 @@ function App() {
   return (
     <Router>
       <div className='blueBG vh-100 col-12 d-flex justify-content-center align-items-center'>
-        <div className='cloudBG'>
-          <nav className='d-flex justify-content-evenly align-items-center'>
-            <Link to='/calendar' className='text-light' onClick={handleCalendarClick}>
-              <input type="radio" className="" name="navOptions" id="option1" checked={calendarChecked} />
-              <label className="text-light" htmlFor="option1">CALENDAR</label>
-            </Link>
-            <Link to='/clients' className='text-light' onClick={handleClientsClick}>
-              <input type="radio" className="" name="navOptions" id="option2" checked={clientsChecked} />
-              <label className="text-light" htmlFor="option2">CLIENTS</label>
-            </Link>
-          </nav>
-          <Routes>
-            <Route exact path='/' element={<CalendarDisplay />} />
-            <Route exact path='/calendar' element={<CalendarDisplay />} />
-            <Route exact path='/clients' element={<Clients />} />
-          </Routes>
-        </div>
+        {loggedIn ?
+          <div className='cloudBG'>
+            <nav className='d-flex justify-content-evenly align-items-center'>
+              <Link to='/calendar' className='text-light' onClick={handleCalendarClick}>
+                <input type="radio" className="" name="navOptions" id="option1" checked={calendarChecked} />
+                <label className="text-light" htmlFor="option1">CALENDAR</label>
+              </Link>
+              <Link to='/clients' className='text-light' onClick={handleClientsClick}>
+                <input type="radio" className="" name="navOptions" id="option2" checked={clientsChecked} />
+                <label className="text-light" htmlFor="option2">CLIENTS</label>
+              </Link>
+            </nav>
+            <Routes>
+              <Route exact path='/' element={<CalendarDisplay />} />
+              <Route exact path='/calendar' element={<CalendarDisplay />} />
+              <Route exact path='/clients' element={<Clients />} />
+            </Routes>
+          </div>
+          :
+          <Login />
+        }
       </div>
     </Router>
   );
---
+++ src/pages/Login.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import auth from "../utils/auth";
+
+const Login = () => {
+    const [username, setusername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleUsernameChange = (e) => {
+        setusername(e.target.value);
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        try {
+            const response = await fetch(`http://localhost:5062/api/login/`, {
+                method: 'POST',
+                body: JSON.stringify({ Username: username, Password: password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+            const data = await response.json();
+            console.log(data.token);
+            auth.login(data.token)
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function getAppointments() {
+    }
+
+    return (
+        <div className="d-flex flex-column align-items-center col-6 text-light fs-3">
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit} className="col-12 d-flex flex-column align-items-center">
+                <div className="mb-3 col-12">
+                    <label htmlFor="username" className="form-label">Username</label>
+                    <input type="username" className="form-control" id="username" aria-describedby="usernameHelp" onChange={handleUsernameChange} />
+                </div>
+                <div className="mb-3 col-12">
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="password" onChange={handlePasswordChange} />
+                </div>
+                <button type="submit" className="custom-btn col-6">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
