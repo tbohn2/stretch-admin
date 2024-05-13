896af8a95c9723afb87b0b6eb183ab75f32fc310
tbohn2 <tanner.bohn@yahoo.com>
2024-05-13 16:47:28 -0700

    Added loading and error display

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import auth from "../utils/auth";
 
 const Login = ({ setLoggedIn }) => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
     const [username, setusername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -15,34 +17,51 @@ const Login = ({ setLoggedIn }) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
+        setError("");
         try {
-            // const response = await fetch(`http://localhost:5062/api/login/`, {
-            const response = await fetch(`https://tbohn2-001-site1.ctempurl.com/api/login/`, {
+            const response = await fetch(`http://localhost:5062/api/login/`, {
+                // const response = await fetch(`https://tbohn2-001-site1.ctempurl.com/api/login/`, {
                 method: 'POST',
                 body: JSON.stringify({ Username: username, Password: password }),
                 headers: { 'Content-Type': 'application/json' },
             });
             const data = await response.json();
-            auth.login(data.token)
-            setLoggedIn(true);
+            setLoading(false);
+            if (data.token) {
+                auth.login(data.token)
+                setLoggedIn(true);
+            }
+            if (!response.ok) {
+                setError(data);
+            }
         } catch (error) {
+            setLoading(false);
+            setError("An error occurred while making request. Please try again later.");
             console.error(error);
         }
     }
 
     return (
-        <div className="d-flex flex-column align-items-center col-6 text-light fs-3">
+        <div className="d-flex flex-column align-items-center col-10 col-md-6 text-light fs-3">
             <h1>Login</h1>
+            {error && <div className="fs-4 alert alert-danger">{error}</div>}
             <form onSubmit={handleSubmit} className="col-12 d-flex flex-column align-items-center">
                 <div className="mb-3 col-12">
                     <label htmlFor="username" className="form-label">Username</label>
-                    <input type="username" className="form-control" id="username" aria-describedby="usernameHelp" onChange={handleUsernameChange} />
+                    <input type="username" className="form-control" id="username" aria-describedby="usernameHelp" onChange={handleUsernameChange} required />
                 </div>
                 <div className="mb-3 col-12">
                     <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" onChange={handlePasswordChange} />
+                    <input type="password" className="form-control" id="password" onChange={handlePasswordChange} required />
                 </div>
-                <button type="submit" className="custom-btn col-6">Login</button>
+                {loading ?
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                    :
+                    <button type="submit" className="custom-btn col-6">Login</button>
+                }
             </form>
         </div>
     );
