418cba7c6de2990aec72697c9d59ded48303178e
tbohn2 <tanner.bohn@yahoo.com>
2024-06-18 16:26:05 -0700

    Created modal for Service; services can be edited

---
+++ src/Components/ServiceModal.jsx
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import auth from '../utils/auth';
+
+const ServicesModal = ({ services, getServices }) => {
+    const token = auth.getToken();
+    const adminId = localStorage.getItem('admin_id');
+
+    const initialFormState = {
+        AdminId: adminId,
+        Id: 0,
+        Name: '',
+        Price: 0,
+        Duration: 0,
+        Description: '',
+        Private: false,
+        Location: '',
+        ImgURL: './assets/defaultService.jpg'
+    };
+
+    const [addingService, setAddingService] = useState(false);
+    const [editingService, setEditingService] = useState(false);
+    const [deletingService, setDeletingService] = useState(false);
+    const [serviceDetails, setServiceDetails] = useState(initialFormState);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setServiceDetails({
+            ...serviceDetails,
+            [name]: value
+        });
+    };
+
+    const clearStates = () => {
+        // clear states
+    }
+
+    const toggleDetails = (service) => {
+        if (serviceDetails.Id === service.Id) {
+            setServiceDetails({});
+        } else {
+            setServiceDetails(service);
+        }
+    }
+
+    const toggleAddService = () => {
+        setAddingService(!addingService);
+    }
+
+    const toggleEditService = () => {
+        setEditingService(!editingService);
+    }
+
+    const toggleDeleteService = () => {
+        setDeletingService(!deletingService);
+    }
+
+    const saveEdit = async () => {
+        setEditingService(false);
+        setLoading(true);
+        try {
+            const response = await fetch(`http://localhost:5062/api/editApptType/`, {
+                method: 'PUT',
+                body: JSON.stringify(serviceDetails),
+                headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` }
+            });
+            if (response.ok) {
+                setLoading(false);
+                localStorage.removeItem('services');
+                getServices();
+            }
+            if (!response.ok) {
+                setLoading(false);
+                setError('Server request failed');
+                console.error('Server request failed');
+            }
+        } catch (error) {
+            setLoading(false);
+            setError('Server request failed');
+            console.error(error);
+        }
+    }
+
+    return (
+        <div className="modal fade" id="servicesModal" tabIndex="-1" aria-labelledby="servicesModalLabel"
+            aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered">
+                <div className="modal-content d-flex justify-content-between">
+                    <div id="modal-header" className="rounded-top p-1 text-center">
+                        <h1 className="modal-title fs-3" id="servicesModalLabel">Services</h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={clearStates}></button>
+                    </div>
+                    <div id="modal-body" className="mt-2 col-12 d-flex flex-column align-items-center flex-grow-1 text-darkgray">
+                        {error && <div className="alert alert-danger">{error}</div>}
+                        {loading && <div className="spinner-border" role="status"></div>}6+
+                        {services.map((service, index) => {
+                            return (
+                                <div className="d-flex flex-wrap justify-content-between border my-2 px-1 fs-4 col-8">
+                                    <div className="service-button col-12 text-center " onClick={() => toggleDetails(service)}>{service.Name}</div>
+                                    {serviceDetails.Id === service.Id && !editingService &&
+                                        <div className={`col-12 d-flex flex-wrap justify-content-center ${serviceDetails && 'fade-in'}`}>
+                                            <div className="col-12">{service.Private ? 'Private' : 'Firm'}</div>
+                                            <div className="col-12">Price: ${service.Price}</div>
+                                            <div className="col-12">Duration: {service.Duration} min</div>
+                                            <div className="col-12">Location:</div>
+                                            <div className="col-12">{service.Location ? service.Location : 'No location specified'}</div>
+                                            <div className="col-12">Description:</div>
+                                            <div className="col-12">{service.Description}</div>
+                                            <button type="button" className="custom-btn col-5 fs-5 m-1" onClick={toggleEditService}>Edit</button>
+                                            <button type="button" className="custom-btn danger-btn col-5 fs-5 m-1" onClick={toggleDeleteService}>Delete</button>
+                                        </div>
+                                    }
+                                    {serviceDetails.Id === service.Id && editingService &&
+                                        <div className={`col-12 d-flex flex-wrap ${serviceDetails && 'fade-in'}`}>
+                                            <div className="col-12">{service.Private ? 'Private' : 'Firm'}</div>
+                                            <div className='col-12 d-flex'>
+                                                <label>Price: $</label>
+                                                <input className='col-2' type='number' name='Price' value={serviceDetails.Price} onChange={handleInputChange}></input>
+                                            </div>
+                                            <div className='col-12 d-flex'>
+                                                <label>Duration:</label>
+                                                <input className='col-2' type='number' name='Duration' value={serviceDetails.Duration} onChange={handleInputChange}></input>
+                                            </div>
+                                            <div>
+                                                <label className="col-12">Location:</label>
+                                                <input type='text' className="col-12" name='Location' value={serviceDetails.Location} onChange={handleInputChange}></input>
+                                            </div>
+                                            <div>
+                                                <label className="col-12">Description:</label>
+                                                <textarea type='text' className="col-12" name='Description' value={serviceDetails.Description} onChange={handleInputChange}></textarea>
+                                            </div>
+                                            <div className='col-12 text-center'>
+                                                <button type="button" className="custom-btn success-btn col-5 fs-5 m-1" onClick={saveEdit}>Save</button>
+                                                <button type="button" className="custom-btn col-5 fs-5 m-1" onClick={toggleEditService}>Cancel</button>
+                                            </div>
+                                        </div>
+                                    }
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div id="modal-footer" className="d-flex align-self-end justify-content-end mt-2">
+                        <button type="button" className="custom-btn fs-5 m-1" onClick={toggleAddService}>Add Service</button>
+                        <button type="button" className="custom-btn fs-5 m-1" data-bs-dismiss="modal" onClick={clearStates}>Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default ServicesModal;
